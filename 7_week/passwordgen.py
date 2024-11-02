97726276d6272244fa3da58dae3d72af74509ab1
FPeterL <feher.peter.lazlo@gmail.com>
2024-11-02 10:00:11 +0100

    fixed the password variable, and used corrent syntax for <=

--- 7_week/passwordgen.py
+++ 7_week/passwordgen.py
@@ -12,14 +12,13 @@ def passwordgen(length):
         char = random.choice(pass_chars)
         password = password + char
 
-    if length =< 8:
+    if length <= 8:
         print("Figyelem: A jelszó túl rövid lehet.")
 
-    return pass_word
+    return password
 
 
 # Nem használt változó
 default_length = 12
 
-vv
 print(passwordgen(10))
